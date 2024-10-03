cf375a223dbc7a7b66b2db807aa3023e3a7ca43a
KSM <ksm0076@gmail.com>
2024-10-03 20:40:56 +0900

    Remove unwanted folders from Git tracking

--- build/my_first_package_msgs/my_first_package_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ksm/ros2_study/build/my_first_package_msgs/my_first_package_msgs__py
-# Build directory: /home/ksm/ros2_study/build/my_first_package_msgs/my_first_package_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_first_package_msgs/rosidl_generator_py/my_first_package_msgs/action/__init__.py
+++
@@ -1,1 +0,0 @@
-from my_first_package_msgs.action._dis_turtle import DisTurtle  # noqa: F401
